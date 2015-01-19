c4926aa1e67dd6e913ef981e6a3833a30b0e2243
zenkr <zenkr@qq.com>
2015-01-19 22:40:32 +0800

    Initial commit

---
+++ app/controllers/GetController.php
@@ -0,0 +1,12 @@
+<?php
+
+class GetController extends BaseController {
+
+    public function contentReturn($dname, $pagepath)
+    {
+        // $arraystr = array();
+        // return Response::json($arraystr);
+        return 'your domain name is'.$dname. '. your path is' .$pagepath;
+    }
+
+}
