1432c4da831a97b1474db54d2c9cdc67ec78fdc8
zenkr <zenkr@qq.com>
2015-01-20 21:52:28 +0800

    json

--- app/controllers/GetController.php
+++ app/controllers/GetController.php
@@ -4,10 +4,8 @@ class GetController extends BaseController {
 
     public function contentReturn($dname, $pagepath = '/')
     {
-    	$conf = yaml_parse_file ('/zsrv/etc/uniuid-tmp/yaml/about.yaml');
-    	print_r($conf);
-        // $arraystr = array();
-        // return Response::json($arraystr);
+        $conf = yaml_parse_file ('/zsrv/etc/uniuid-tmp/yaml/about.yaml');
+        return Response::json($conf);
         return 'your domain name is '.$dname. '. your path is ' . $pagepath;
         // return 'your domain name is'.$dname. '. your path is' . Request::path();
     }
